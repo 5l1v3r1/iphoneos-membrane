64d13a0eacedca3f5975261a828b985e0ba02457
Ivan Nikolsky <enty8080@gmail.com>
2021-05-07 19:57:11 +0300

    Update membrane.h

--- dylib/src/membrane.h
+++ dylib/src/membrane.h
@@ -25,31 +25,87 @@
 #import "rocketbootstrap.h"
 
 @interface UIApplication (Undocumented)
-
 -(bool)launchApplicationWithIdentifier:(id)arg1 suspended:(bool)arg2;
-
 @end
 	
-@interface CLLocationManager (Undocumented)
+@interface SBMediaController : NSObject {
+    int _manualVolumeChangeCount;
+    float _pendingVolumeChange;
+    NSTimer* _volumeCommitTimer;
+    BOOL _debounceVolumeRepeat;
+    NSDictionary *_nowPlayingInfo;
+}
 
-+(void)setLocationServicesEnabled:(BOOL)arg1;
+@property (assign, getter=isRingerMuted, nonatomic) BOOL ringerMuted;
++ (id)sharedInstance;
+- (void)setRingerMuted:(BOOL)arg1;
+- (void)cancelLockScreenIdleTimer;
+- (void)turnOnScreenFullyWithBacklightSource:(int)arg1;
+- (BOOL)play;
+- (BOOL)togglePlayPause;
+- (BOOL)isPlaying;
+- (BOOL)changeTrack:(int)track;
+@end
 
+@interface SpringBoard
++ (id)sharedInstance;
+- (void)_simulateLockButtonPress;
+@end
+
+@interface SBIcon : NSObject
+- (NSString *)nodeIdentifier;
+@end
+
+
+@interface SBApplicationIcon : SBIcon
+@end
+
+@interface SBIconController : NSObject
+- (id)lastTouchedIcon;
 @end
 
 @interface SBUserAgent : NSObject
++ (id)sharedUserAgent;
+- (void)lockAndDimDevice;
+- (void)handleMenuDoubleTap;
+- (void)clickedMenuButton;
+- (bool)handleHomeButtonSinglePressUp;
+- (bool)handleHomeButtonDoublePressDown;
+@end
 
-+(id)sharedUserAgent;
--(void)handleMenuDoubleTap;
--(void)clickedMenuButton;
--(bool)handleHomeButtonSinglePressUp;
--(bool)handleHomeButtonDoublePressDown;
+@interface SBDeviceLockController : NSObject
++ (id)sharedController;
+- (void)_clearBlockedState;
+- (BOOL)isPasscodeLocked;
+@end
 
+@interface CLLocationManager : NSObject
++ (void)setLocationServicesEnabled:(BOOL)arg1;
 @end
 
 @interface SBLockScreenManager : NSObject
-
 @property (nonatomic, readonly) BOOL isUILocked;
++ (id)sharedInstance;
+- (BOOL)attemptUnlockWithPasscode:(id)passcode;
+@end
 
-+(id)sharedInstance;
+@interface SBHUDController : NSObject
++ (id)sharedInstance;
+- (void)hideHUD;
+- (void)showHUD;
+@end
 
+@interface VolumeControl : NSObject
++ (id)sharedVolumeControl;
+- (void)toggleMute;
+@end
+
+@interface SBRingerControl : NSObject
+- (BOOL)isRingerMuted;
+-(void)setRingerMuted:(BOOL)arg1;
+@end
+
+@interface SBMainWorkspace : NSObject
++ (SBMainWorkspace *)sharedInstance;
+@property (readonly, nonatomic) SBRingerControl *ringerControl;
 @end
